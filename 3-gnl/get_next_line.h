2198d90f864046b6bdac8b4de435709754b45d7e
Gilbert Sahyoun <gilbertsahyoun.business@gmail.com>
2025-10-27 13:21:27 -0400

    latest

---
+++ 3-gnl/get_next_line.h
@@ -0,0 +1,16 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+int	    ft_strlen(char *s);
+int     ft_strchr(const char *s, char c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
